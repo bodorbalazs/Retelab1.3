834336f86141bf73d439719bd73f9dfcabc8fa82
Bödör Balázs <bodorbalazs44@gmail.com>
2021-03-08 16:14:29 +0100

    error implemented

--- train-controller/src/main/java/hu/bme/mit/train/controller/Main.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("Welcome to the epic train!");
         TrainControllerImpl TrainControl = new TrainControllerImpl();
         TrainControl.setSpeedLimit(222);
-        hehe
+
     }
 
 
