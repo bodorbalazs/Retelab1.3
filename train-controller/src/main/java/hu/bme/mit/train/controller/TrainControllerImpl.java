002aee8b4dca30cb88b68901f93abcc957108fc1
Bödör Balázs <bodorbalazs44@gmail.com>
2021-03-28 13:29:07 +0200

    implemented timer

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -3,31 +3,29 @@ package hu.bme.mit.train.controller;
 import com.google.common.collect.Table;
 import hu.bme.mit.train.interfaces.TrainController;
 
-public class TrainControllerImpl  extends Thread implements TrainController {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
 	private Tachograph Tachocounter = new Tachograph();
 	private int time=0;
-	private Thread thread;
+	private Timer timer;
 
 
 	public TrainControllerImpl(){
-		TrainControllerImpl obj = new TrainControllerImpl();
-		thread = new Thread();
-		this.run();
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				followSpeed();
+			}
+		},1000,5000);
 		}
 
-	public void run(){
-		thread.run();
-		followSpeed();
-		try {
-			thread.sleep(2500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
 
 
 
